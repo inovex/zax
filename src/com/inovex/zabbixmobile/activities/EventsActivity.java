05cc416a6dca8fc4cb661ddb884921255ebcc49b
Johannes Schamburger <jschamburger@inovex.de>
2013-06-07 16:40:06 +0200

    Added Ormlite infrastructure.

--- src/com/inovex/zabbixmobile/activities/EventsActivity.java
+++ src/com/inovex/zabbixmobile/activities/EventsActivity.java
@@ -1,17 +1,44 @@
 package com.inovex.zabbixmobile.activities;
 
+import java.sql.SQLException;
+import java.util.Random;
+
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.actionbarsherlock.view.MenuItem;
 import com.inovex.zabbixmobile.R;
+import com.inovex.zabbixmobile.model.DatabaseHelper;
+import com.inovex.zabbixmobile.model.Event;
+import com.j256.ormlite.android.apptools.OpenHelperManager;
+import com.j256.ormlite.dao.Dao;
 
 public class EventsActivity extends SherlockFragmentActivity {
-	
+
 	private static final String TAG = EventsActivity.class.getSimpleName();
-	
+
+	private DatabaseHelper databaseHelper = null;
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		if (databaseHelper != null) {
+			OpenHelperManager.releaseHelper();
+			databaseHelper = null;
+		}
+	}
+
+	private DatabaseHelper getHelper() {
+		if (databaseHelper == null) {
+			databaseHelper = OpenHelperManager.getHelper(this,
+					DatabaseHelper.class);
+		}
+		return databaseHelper;
+	}
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -22,13 +49,41 @@ public class EventsActivity extends SherlockFragmentActivity {
 		actionBar.setHomeButtonEnabled(true);
 		actionBar.setDisplayHomeAsUpEnabled(true);
 		actionBar.setDisplayShowTitleEnabled(true);
-		
-		LinearLayout baseLayout = (LinearLayout)findViewById(R.id.layout_events);
-		
+
+		LinearLayout baseLayout = (LinearLayout) findViewById(R.id.layout_events);
+
 		TextView textView = new TextView(this);
 		textView.setText("Events activity");
 		baseLayout.addView(textView);
 		
+		getHelper();
+		
+		try {
+			TextView textView2 = new TextView(this);
+			doAccountDatabaseStuff(textView2);
+			baseLayout.addView(textView2);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+	
+	private void doAccountDatabaseStuff(TextView tv) throws SQLException {
+
+		// clear database
+//		databaseHelper.onUpgrade(databaseHelper.getWritableDatabase(), 0, 0);
+		
+		StringBuilder sb = new StringBuilder();
+		
+		Event e = new Event(databaseHelper, System.currentTimeMillis());
+
+		Dao<Event, Integer> eventDao = databaseHelper.getDao(Event.class);
+
+		for (Event event : eventDao) {
+			sb.append(event + "\n");
+		}
+		tv.setText(sb.toString());
 	}
 
 	@Override
@@ -44,5 +99,5 @@ public class EventsActivity extends SherlockFragmentActivity {
 		}
 		return false;
 	}
-	
+
 }
