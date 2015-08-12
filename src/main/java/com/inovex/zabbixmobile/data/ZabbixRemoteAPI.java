541a610aab8af8f390cc32e5a8e5ba547805c0c0
Christopher Jakob <jakob.chrisli@gmail.com>
2015-08-12 13:13:14 +0200

    Fix crash urls with backslash
    If an url is entered with backslashes instead of slashes the application crashes.
    Alternative to the manual validation should be: Patterns.WEB_URL.matcher(potentialUrl).matches()

--- src/main/java/com/inovex/zabbixmobile/data/ZabbixRemoteAPI.java
+++ src/main/java/com/inovex/zabbixmobile/data/ZabbixRemoteAPI.java
@@ -1976,7 +1976,9 @@ public class ZabbixRemoteAPI {
 				R.string.url_example)
 				+ (mContext.getResources().getString(R.string.url_example)
 						.endsWith("/") ? "" : '/') + API_PHP;
-		if (zabbixUrl == null || zabbixUrl.equals(exampleUrl) || zabbixUrl.startsWith("https:///")) {
+		if (zabbixUrl == null || zabbixUrl.equals(exampleUrl) || zabbixUrl.startsWith("https:///")
+				|| zabbixUrl.contains("\\"))
+		{
 			throw new FatalException(Type.SERVER_NOT_FOUND);
 		}
 	}
