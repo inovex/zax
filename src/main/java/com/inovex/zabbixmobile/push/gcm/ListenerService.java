a0f19935f35033f5dcf08e6b24508fb160297d1a
Felix Bürkle <f.buerkle@inovex.de>
2015-10-12 18:21:57 +0200

    update manifest for GCM

---
+++ src/main/java/com/inovex/zabbixmobile/push/gcm/ListenerService.java
@@ -0,0 +1,9 @@
+package com.inovex.zabbixmobile.push.gcm;
+
+import com.google.android.gms.iid.InstanceIDListenerService;
+
+/**
+ * Created by felix on 12/10/15.
+ */
+public class ListenerService extends InstanceIDListenerService{
+}
