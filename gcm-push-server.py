d55076cc277071a27a5db06fbc70a8042adf5153
Felix Bürkle <f.buerkle@inovex.de>
2015-11-03 16:47:12 +0100

    implement sending of messages to google-server

--- gcm-push-server.py
+++ gcm-push-server.py
@@ -3,9 +3,14 @@
 import json
 import sqlite3
 import copy
+
+from gcmclient import *
 from wsgiref.simple_server import make_server
 
 from cgi import parse_qs, escape
+
+API_KEY = ""
+DRY_RUN = True
 
 def application (environ, start_response):
 	try:
@@ -37,14 +42,15 @@ def application (environ, start_response):
 		if d['action'] == 'send': # send message
 			cur.execute('SELECT registration_id FROM registration_ids')
 			res = cur.fetchall()
-			response['recp'] = [r[0] for r in res]
-			response['status'] = 'success'
+			recipients = [r[0] for r in res]
+			response['recp'] = recipients
 			msg = copy.deepcopy(d)
 			del msg['action']
 			if 'registrationID' in d.keys():
 				del msg['registrationID']
 			response['message'] = msg
 			# TODO send message to GCM-Server
+			response['status'] = send_message(recipients,msg,cur)
 
 	con.commit()
 	con.close()
@@ -57,6 +63,52 @@ def application (environ, start_response):
 	start_response(status,response_headers)
 	return [response_body]
 
+def send_message(recipients,message,cursor):
+	status = ""
+	if API_KEY != "" :
+		gcm = GCM(API_KEY)
+		message = JSONMessage(recipients, message, dry_run = DRY_RUN)
+		try:
+			response = gcm.send(message)
+			for reg_id, msg_id in res.success.items():
+				print("Successfully sent %s as %s" % (reg_id, msg_id))
+
+			for reg_id, new_reg_id in res.canonical.items():
+				print("Replacing %s with %s in database" % (reg_id, new_reg_id))
+				cur.execute('DELETE FROM registration_ids WHERE (registration_id == ?)', (reg_id,))
+				ur.execute('INSERT INTO registration_ids VALUES (?)', (new_reg_i,d))
+
+			for reg_id in res.not_registered:
+				print("Removing %s from database" % reg_id)
+				cur.execute('DELETE FROM registration_ids WHERE (registration_id == ?)', (regid,))
+
+			# unrecoverably failed, these ID's will not be retried
+			# consult GCM manual for all error codes
+			for reg_id, err_code in res.failed.items():
+				print("Removing %s because %s" % (reg_id, err_code))
+				cur.execute('DELETE FROM registration_ids WHERE (registration_id == ?)', (regid,))
+
+			# if some registration ID's have recoverably failed
+			if res.needs_retry():
+				# construct new message with only failed regids
+				retry_msg = res.retry()
+				# you have to wait before attemting again. delay()
+				# will tell you how long to wait depending on your
+				# current retry counter, starting from 0.
+				# TODO retry sending message
+				print("Wait or schedule task after %s seconds" % res.delay(retry))
+				# retry += 1 and send retry_msg again
+		except GCMAuthenticationError:
+			status = "Your Google API key is rejected"
+		except ValueError, e:
+			status = "Invalid message/option or invalid GCM response" + e.args[0]
+		except Exception:
+			status = "Something wrong with requests library"
+	else:
+		status = "Google API key is required"
+	return status
+
+# run locally for testing and development
 if __name__ == "__main__":
 	httpd = make_server('localhost', 8080, application)
 	httpd.serve_forever()
