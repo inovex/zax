a0c1353e7468e34bd4daf67576e8d8795ce17a51
Felix Bürkle <f.buerkle@inovex.de>
2015-11-23 12:34:13 +0100

    gcm.pl: make logging configurable in config-section

--- gcm.pl
+++ gcm.pl
@@ -9,25 +9,30 @@ use LWP::UserAgent;
 ######################
 # config
 my $url = "http://localhost/push-server";
-my $logfile = "/tmp/gcm.log"
 
+my $debug = 1;
+my $logfile = "/var/log/zabbix/gcm.log";
+my $csv = "/var/log/zabbix/gcm_push.csv"; # set to 0 to disable logging events to csv
 # end config
 ######################
 
 use Data::Dumper;
 use URI::Escape;
 
-
 open(my $log, ">>", $logfile);
 flock $log, 2;
-my $datestring = localtime();
-print $log $datestring . "\n";
 
-#print $log Dumper(\@ARGV);
+my $datestring = localtime();
+if($debug){
+    print $log $datestring . "\n";
+}
 
 my @lines = split(/\n/, $ARGV[2]);
 
-print $log Dumper(\@lines);
+if($debug){
+#    print $log Dumper(\@ARGV);
+    print $log Dumper(\@lines);
+}
 
 my %msg = ();
 for my $l (@lines) {
@@ -38,17 +43,13 @@ for my $l (@lines) {
 	$msg{$key} = $val;
 }
 
-#print $log Dumper(\%msg);
-
-#$url .= uri_escape("{\"triggerid\": " . $ARGV[1] . ", \"message\": \"" . $ARGV[2] . "\"}");
 my $json = "{\"triggerid\": " . $ARGV[1] . ", \"message\": \"" . $msg{"message"} . "\",\"status\": \"" . $msg{"status"} . "\"}";
 
-#print $log "$url\n";
-print $log "\n";
-print $log "$json\n";
-print $log "\n";
-#print $log post($url, $json);
-#print $log "\n------------------------------------\n\n";
+if($debug){
+    print $log "$url\n";
+    print $log "$json\n";
+#    print $log "\n------------------------------------\n\n";
+}
 
 my $ua = LWP::UserAgent->new;
 my $req = HTTP::Request->new(POST => $url);
@@ -62,9 +63,16 @@ if ($response->is_success) {
     print "Received reply: $message\n";
 }
 else {
-    print "HTTP POST error code: ", $response->code, "\n";
-    print "HTTP POST error message: ", $response->message, "\n";
+    print $log "HTTP POST error code: ", $response->code, "\n";
+    print $log "HTTP POST error message: ", $response->message, "\n";
 }
 
 close($log);
 
+# DEBUG Logging each push-message into a csv-file
+if($csv){
+    open(my $pushlog, ">>", $csv);
+    flock $pushlog,2;
+    print $pushlog $datestring . "\t" . $ARGV[1] . "\t" . $msg{"status"} . "\t" . $msg{"message"} . "\n";
+    close($pushlog);
+}
